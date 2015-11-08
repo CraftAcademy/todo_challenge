e5ab67a63c6bc22d0b9764e99697c3881080a02a
Christopher Comella <git.chrisco@gmail.com>
2015-11-08 15:07:07 +0100

    WIP: create extremely minimal 'to-do list' Angular app Next steps: * Feature: Ability to edit to-dos (maybe with https://vitalets.github.io/angular-xeditable/) * Feature: Ability to delete to-dos * Style with Bootstrap * Write tests * Feature: Persist data (local and/or remote) * Deploy to world * Feature: Visitors can sign up and become Users

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
-angular.module('ToDo', []).controller('toDoController', [
-    function() {
+angular.module('ToDo', []).
+controller('toDoController', ['$scope',
+
+    function($scope) {
+
+        $scope.todos = [{
+            'title': "Enter to-dos",
+            'completed': false
+        }];
+
+        $scope.addToDo = function() {
+            $scope.todos.push({
+                'title': $scope.newToDo,
+                'completed': false
+            });
+            $scope.newToDo = '';
+        };
+
+        $scope.clearCompleted = function() {
+            $scope.todos = $scope.todos.filter(function(todo) {
+                return !todo.completed;
+            });
+        };
     }
 ]);
